d35f1a940abe84e365fe2c0baacda84d1767d802
Carlos Henrique <Carlos.gerasoft@gmail.com>
2025-07-31 20:54:06 -0300

    Versão inicial

---
+++ deploy.bat
@@ -0,0 +1,26 @@
+@echo off
+echo ========== BUILDANDO O PROJETO ==========
+npm run build
+
+echo.
+echo ========== INDO PARA A PASTA DIST ==========
+cd dist
+
+echo.
+echo ========== INICIALIZANDO GIT ==========
+git init
+git branch -M main
+git remote add origin https://github.com/carlos-hds/HTMLDinamico-Public.git
+
+echo.
+echo ========== COMMITANDO ==========
+git add .
+git commit -m "Deploy automático"
+
+echo.
+echo ========== ENVIANDO PARA O GITHUB PAGES ==========
+git push -f origin main
+
+echo.
+echo ✅ Deploy finalizado com sucesso!
+pause
