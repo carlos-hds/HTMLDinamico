ff93ba7f8f137aaa4c5eb2a6a6070a0069996f04
Carlos Henrique <126909691+carlos-hds@users.noreply.github.com>
2025-07-30 22:40:13 -0300

    Update vite.config.ts
    Signed-off-by: Carlos Henrique <126909691+carlos-hds@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/block-html-builder/",
   server: {
     host: "::",
     port: 8080,
