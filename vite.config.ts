cc76e9d41fbc747214629580b3aa64ed2dfc70b2
Carlos Henrique <Carlos.gerasoft@gmail.com>
2025-07-31 23:58:08 -0300

    Build ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
- import { defineConfig } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
@@ -6,14 +6,17 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "/HTMLDinamico/",
+  build: {
+    outDir: "docs", // <--- necessário para GitHub Pages
+    emptyOutDir: true,
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
